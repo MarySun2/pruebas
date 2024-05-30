4ec534c864be208efa9b153f05de0580e025486d
Marisol Pereira <mpg0041@alu.medac.es>
2024-05-30 15:39:12 +0200

    x

--- Js/app.js
+++ Js/app.js
@@ -6,13 +6,13 @@ console.log("Agregado");
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://support.google.com/firebase/answer/7015592
 const firebaseConfig = {
-    apiKey: "AIzaSyDm-pUlENsgifuh5l_JWh8qcPmgdTV6Ew0",
-    authDomain: "prueba1-cbd49.firebaseapp.com",
-    projectId: "prueba1-cbd49",
-    storageBucket: "prueba1-cbd49.appspot.com",
-    messagingSenderId: "642390832379",
-    appId: "1:642390832379:web:16e4e533d631f9994aac5e",
-    measurementId: "G-RKETW3YWGZ"
+    apiKey: "AIzaSyD785S6cHJjqRxHLtJ3zugA6cHutfy_9EM",
+    authDomain: "prueba2-ebd2e.firebaseapp.com",
+    projectId: "prueba2-ebd2e",
+    storageBucket: "prueba2-ebd2e.appspot.com",
+    messagingSenderId: "665315151798",
+    appId: "1:665315151798:web:f9015e5b4340965edf3b17",
+    measurementId: "G-DWN48GL4W9"
   };
 
 // Initialize Firebase
