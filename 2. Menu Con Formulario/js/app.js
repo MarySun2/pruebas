ea6694e77d96fb52b95672a581cf429a163633f8
Marisol Pereira <mpg0041@alu.medac.es>
2024-06-05 19:27:34 +0200

    PDF+FORMULARIO

--- 2. Menu Con Formulario/js/app.js
+++ 2. Menu Con Formulario/js/app.js
@@ -2,3 +2,34 @@
     var selectedValue = this.value;
     document.getElementById('displayImage').src = selectedValue;
     });
+
+    // PDF
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Escuchamos el click del botón
+    const $boton = document.querySelector("#btnCrearPdf");
+    $boton.addEventListener("click", () => {
+        const $elementoParaConvertir = document.body; // <-- Aquí puedes elegir cualquier elemento del DOM
+        html2pdf()
+            .set({
+                margin: 1,
+                filename: 'documento.pdf',
+                image: {
+                    type: 'jpeg',
+                    quality: 0.98
+                },
+                html2canvas: {
+                    scale: 3, // A mayor escala, mejores gráficos, pero más peso
+                    letterRendering: true,
+                },
+                jsPDF: {
+                    unit: "in",
+                    format: "a3",
+                    orientation: 'portrait' // landscape o portrait
+                }
+            })
+            .from($elementoParaConvertir)
+            .save()
+            .catch(err => console.log(err));
+    });
+});
