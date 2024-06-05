37f384ab448eaabf6270f31393f94bfcd00bbc2f
Marisol Pereira <mpg0041@alu.medac.es>
2024-06-05 13:20:00 +0200

    inicial

--- 2. Ejemplofirma/js/documento.js
+++
@@ -1,5 +0,0 @@
-if (window.opener) {
-    document.querySelector("#firma").src = window.opener.obtenerImagen();
-    // Imprimir documento. Si no quieres imprimir, remueve la siguiente línea
-    window.print();
-}
